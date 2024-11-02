fb5b0a0b522f914af3332d997996c5ee6f2c288f
manojsinghh <2manojsinghh@gmail.com>
2024-11-02 12:49:04 +0530

    variable_task_added

---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        //variable
+        int a=25;
+        int b=5;
+        int sum =a+b;
+        int diff =a-b;
+        int mul =a*b;
+        int div =a/b;
+        System.out.println(sum);
+        System.out.println(diff);
+        System.out.println(mul);
+        System.out.println(div);
+
+    }
+}
