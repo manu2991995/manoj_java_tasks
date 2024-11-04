e5ddb726b70a3d7faa69c3ac6c35e97bff9b9925
manojsinghh <2manojsinghh@gmail.com>
2024-11-04 06:30:22 +0530

    Caluclator /operators task is addede

---
+++ Caluclator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Caluclator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+5
+        // Taking user inputs
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        // Performing calculations
+        double addition = num1 + num2;
+        double subtraction = num1 - num2;
+        double multiplication = num1 * num2;
+        double division = num1 / num2;
+        double modulus = num1 % num2;
+
+        // Displaying results
+        System.out.println("Results:");
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
+        System.out.println("Modulus: " + modulus);
+
+        scanner.close();
+    }
+
+}
