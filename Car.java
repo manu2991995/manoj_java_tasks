46f7257e9f461f9a786b76132f441b1dfd8af4ca
manojsinghh <2manojsinghh@gmail.com>
2024-11-05 16:50:05 +0530

    sample

--- Car.java
+++ Car.java
@@ -1,20 +1,20 @@
-public class Car {
-    String colour, model;
-    int year;
+//public class Car {
+  //  String colour, model;
+    //int year;
 
-    public static void main(String[] args) {
-        Car a1 = new Car();
-        a1.colour="Black";
-        a1.model="Mahindra_Thar";
-        a1.year=2024;
+    //public static void main(String[] args) {
+      //  Car a1 = new Car();
+        //a1.colour="Black";
+        //a1.model="Mahindra_Thar";
+        //a1.year=2024;
 
-        System.out.println(a1.model + "-->" + a1.colour + "-->" + a1.year);
-
-
+        //System.out.println(a1.model + "-->" + a1.colour + "-->" + a1.year);
 
 
 
 
 
-    }
-}
+
+
+    //}
+//}
